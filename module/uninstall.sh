aafbf43b86d847024f06cfeef1ed9ac928f7a64f
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-10 18:53:53 +0700

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,5 +1,8 @@
 #!/system/bin/sh
 
+MODPATH=/data/adb/modules/zapret
+MODUPDATEPATH=/data/adb/modules_update/zapret
+
 su -c "pkill -f '/data/adb/service.d/zapret.sh'"
 su -c "pkill -f '${MODDIR}/zapret.sh'"
 su -c "pkill -f '${MODDIR}/service.sh'"
