d2731069d89daad64b138576d110fd97bac9d778
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-10 16:52:57 +0700

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,6 +1,8 @@
 #!/system/bin/sh
 
-su -c "kill $(pgrep -f '/data/adb/service.d/zapret.sh')"
+su -c "pkill -f '/data/adb/service.d/zapret.sh'"
+su -c "pkill -f '${MODDIR}/zapret.sh'"
+su -c "pkill -f '${MODDIR}/service.sh'"
 su -c 'pkill nfqws'
 su -c 'pkill zapret'
 su -c 'iptables -t mangle -F PREROUTING'
