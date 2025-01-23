122d5cffadaccedadfe537b64413a0e46fe62fe2
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-23 19:13:41 +0700

    Update bol-van.sh

--- module/tactics/bol-van.sh
+++ module/tactics/bol-van.sh
@@ -6,7 +6,7 @@ hostlist="--hostlist=$MODPATH/list.txt"
 
 # Zapret Configuration
 config="--filter-tcp=80 $autohostlist --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new"
-config="--filter-tcp=80 $hostlist --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new"
+config="$config --filter-tcp=80 $hostlist --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new"
 config="$config --filter-tcp=443 $hostlist --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=$MODPATH/tls.bin --new"
 config="$config --filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=$MODPATH/quic.bin --new"
 config="$config --filter-udp=443 $autohostlist --dpi-desync=fake --dpi-desync-repeats=11 --new"
