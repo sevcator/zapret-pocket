c92b6b0a98901971cf19bd056b432dace0a85072
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-23 15:35:20 +0700

    Update action.sh

--- module/action.sh
+++ module/action.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
-# t.me/sevcator 0_o Tuno Diagnostic Tyl dlya zapreta XD
-
 MODPATH=/data/adb/modules/zapret
 
 echo "***********************"
-echo "zapret Diagnostic Tool"
+echo "*       zapret        *"
 echo "***********************"
 
-if pgrep -f "$MODPATH/zapret.sh" > /dev/null || pgrep -f "$MODPATH/service.sh" > /dev/null; then
-    echo "- Service script is runnning"
-    echo "- Terminating process"
-    pkill nfqws
-else
-    echo "- Terminating process"
-    pkill nfqws
-    echo "- Service script is not runnning"
-
-    echo "- Starting service"
-    $MODPATH/service.sh &
-
-    if [ $? -eq 0 ]; then
-        echo "- The service started"
-    else
-        echo "! Failed to run start service"
-    fi
-fi
+su -c "$MODPATH/system/bin/zapret toggle"
