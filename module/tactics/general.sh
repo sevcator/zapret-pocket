dfd54751b75d9419061f7e287c29d55a48844ad3
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-23 15:08:27 +0700

    Create general.sh

---
+++ module/tactics/general.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Values
+hostlist="--hostlist-exclude=$MODPATH/list-exclude.txt --hostlist-auto=$MODPATH/list.txt"
+
+# Zapret Configuration
+config="--filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/quic.bin --new"
+config="$config --filter-udp=50000-50100 --ipset=$MODPATH/ipset-discord.txt --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new"
+config="$config --filter-tcp=80 $hostlist --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new"
+config="$config --filter-tcp=443 $hostlist --dpi-desync=fake,split --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=$MODPATH/tls.bin"
