2e524995eae9046ceaf4808b5203988836d85fa2
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-24 21:42:50 +0700

    Create hosts-flowseal-alt-4.sh

---
+++ module/tactics/hosts-flowseal-alt-4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Values
+hostlist="--hostlist=$MODPATH/list.txt"
+
+# Zapret Configuration
+config="$config --filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/quic.bin --new"
+config="$config --filter-udp=50000-50100 --ipset=$MODPATH/ipset-discord.txt --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new"
+config="$config --filter-tcp=80 $hostlist --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new"
+config="$config --filter-l3=ipv4 --filter-tcp=443 --dpi-desync=syndata"
