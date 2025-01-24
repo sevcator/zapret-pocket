c3be2c5691e1bd9ebd3f0c9f7d16b2c964a669b7
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-24 21:38:07 +0700

    Create hosts-bol-van.sh

---
+++ module/tactics/hosts-bol-van.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Values
+hostlist="--hostlist=$MODPATH/list.txt"
+
+# Zapret Configuration
+config="--filter-tcp=80 $hostlist --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new"
+config="$config --filter-tcp=443 $hostlist --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=$MODPATH/tls.bin --new"
+config="$config --filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=$MODPATH/quic.bin --new"
+config="$config --filter-udp=50000-50099 --ipset=$MODPATH/ipset-discord.txt --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-any-protocol --dpi-desync-cutoff=n4 --new"
