62d2ba3a518ca76c9f4870fd45cc5bdba9bf4820
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-25 10:05:35 +0700

    Add files via upload

--- module/tactics/autohosts-flowseal-alt.sh
+++ module/tactics/autohosts-flowseal-alt.sh
@@ -6,7 +6,7 @@ hostlist="--hostlist=$MODPATH/list.txt"
 
 # Zapret Configuration
 config="--filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig $autohostlist --new"
-config="$config --filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/quic.bin --new"
+config="$config --filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/quic-google.bin --new"
 config="$config --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 $autohostlist --new"
 config="$config --filter-udp=50000-50100 --ipset=$MODPATH/ipset-discord.txt --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new"
 config="$config --filter-tcp=443 $hostlist --dpi-desync=fake,split --dpi-desync-autottl=5 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq"
--- module/tactics/hosts-flowseal-mgts.sh
+++ module/tactics/hosts-flowseal-mgts.sh
@@ -4,7 +4,7 @@
 hostlist="--hostlist=$MODPATH/list.txt"
 
 # Zapret Configuration
-config="--filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/quic.bin --new"
+config="--filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/quic-google.bin --new"
 config="$config --filter-udp=50000-50100 --ipset=$MODPATH/ipset-discord.txt --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new"
 config="$config --filter-tcp=80 $hostlist --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new"
-config="$config --filter-tcp=443 $hostlist --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=$MODPATH/tls.bin"
+config="$config --filter-tcp=443 $hostlist --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=$MODPATH/tls-google.bin"
