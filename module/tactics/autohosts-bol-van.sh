134797911221626c50557ddcb939a3dea51e7a29
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-25 08:19:24 +0700

    Create autohosts-bol-van.sh

---
+++ module/tactics/autohosts-bol-van.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Values
+autohostlist="--hostlist-auto=$MODPATH/list-auto.txt"
+hostlist="--hostlist=$MODPATH/list.txt"
+
+# Zapret Configuration
+config="--filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig $autohostlist --new"
+config="$config --filter-tcp=443 $hostlist --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=$MODPATH/tls.bin --new"
+config="$config --filter-tcp=443 --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq,md5sig $autohostlist --new"
+config="$config --filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=$MODPATH/quic.bin --new"
+config="$config --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 $autohostlist --new"
+config="$config --filter-udp=50000-50099 --ipset=$MODPATH/ipset-discord.txt --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-any-protocol --dpi-desync-cutoff=n4 --new"
