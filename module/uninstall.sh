c06294838717f67201e67b55a5590d3d01724068
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-25 09:13:21 +0700

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -3,15 +3,13 @@
 MODPATH=/data/adb/modules/zapret
 MODUPDATEPATH=/data/adb/modules_update/zapret
 
-sysctl net.netfilter.nf_conntrack_tcp_be_liberal=1 > /dev/null;
-sysctl net.ipv6.conf.all.disable_ipv6=1 > /dev/null;
-sysctl net.ipv6.conf.default.disable_ipv6=1 > /dev/null;
-sysctl net.ipv6.conf.lo.disable_ipv6=1 > /dev/null;
-for pid in $(grep -l "zapret.sh" /proc/*/cmdline | awk -F'/' '{print $3}'); do
-    su -c "pkill -9 $pid"
+sysctl -w net.netfilter.nf_conntrack_tcp_be_liberal=1 > /dev/null;
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 > /dev/null;
+sysctl -w net.ipv6.conf.default.disable_ipv6=1 > /dev/null;
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1 > /dev/null;
+for pid in $(pgrep -f zapret.sh); do
+    kill -9 $pid
 done
-su -c "pkill -f '${MODDIR}/zapret.sh'"
-su -c "pkill -f '${MODDIR}/service.sh'"
 su -c 'pkill nfqws'
 su -c 'pkill zapret'
 su -c 'iptables -t mangle -F PREROUTING'
