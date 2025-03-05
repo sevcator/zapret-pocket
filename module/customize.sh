7ed2d365bd0d4791877a45ee5a0ff3d0632d522e
Seva <68725282+sevcator@users.noreply.github.com>
2025-03-05 19:47:02 +0700

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -65,6 +65,9 @@ if [ -f "$MODPATH/current-tactic" ]; then
     fi
 fi
 
+for pid in $(pgrep -f zapret.sh); do
+    kill -9 $pid
+done
 su -c 'pkill nfqws'
 su -c 'pkill zapret'
 su -c 'iptables -t mangle -F PREROUTING'
