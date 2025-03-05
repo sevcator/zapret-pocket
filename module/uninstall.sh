0130ce692d010fb695dbe646679c6d22af868e8b
Seva <68725282+sevcator@users.noreply.github.com>
2025-03-05 19:47:21 +0700

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,13 +1,5 @@
 #!/system/bin/sh
 
-MODPATH=/data/adb/modules/zapret
-MODUPDATEPATH=/data/adb/modules_update/zapret
-
-content update --uri content://telephony/carriers --bind protocol:s:IPV4V6 --bind roaming_protocol:s:IPV4V6 --where "current=1" > /dev/null;
-sysctl -w net.netfilter.nf_conntrack_tcp_be_liberal=1 > /dev/null;
-sysctl -w net.ipv6.conf.all.disable_ipv6=1 > /dev/null;
-sysctl -w net.ipv6.conf.default.disable_ipv6=1 > /dev/null;
-sysctl -w net.ipv6.conf.lo.disable_ipv6=1 > /dev/null;
 for pid in $(pgrep -f zapret.sh); do
     kill -9 $pid
 done
