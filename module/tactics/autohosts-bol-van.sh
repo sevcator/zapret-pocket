844ed68341f47be0b6c897bc50fab4af874359e7
Seva <68725282+sevcator@users.noreply.github.com>
2025-03-16 12:04:24 +0700

    Update autohosts-bol-van.sh

--- module/tactics/autohosts-bol-van.sh
+++ module/tactics/autohosts-bol-van.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Default zapret configuration (https://github.com/bol-van/zapret-win-bundle)
 
 # Values
 autohostlist="--hostlist-exclude=$MODPATH/list-exclude.txt --hostlist-auto=$MODPATH/list-auto.txt"
