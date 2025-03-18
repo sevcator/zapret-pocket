28680894dcd53c5caff30368bf2eff7d51623907
Seva <68725282+sevcator@users.noreply.github.com>
2025-03-18 11:37:43 +0700

    Update action.sh

--- module/action.sh
+++ module/action.sh
@@ -2,9 +2,8 @@
 MODPATH=/data/adb/modules/zapret
 
 echo "**********************************"
-echo "*             zapret             *"
+echo "*         zapret-magisk          *"
 echo "**********************************"
-echo ! The button 'Aciton' is only toggling zapret!
-echo ! If you need the config zapret, open Terminal, type su and type zapret
-echo 
+echo ! Note: The button Action only toggle zapret
+echo ! To the configure, call zapret in Terminal
 su -c "$MODPATH/system/bin/zapret toggle"
