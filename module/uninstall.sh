3fd70f591f7f0b1672a65ffc79d3038ec2d8ab81
Seva <68725282+sevcator@users.noreply.github.com>
2025-03-18 11:12:14 +0700

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,15 +1,17 @@
 #!/system/bin/sh
 
-# Enable IPv6, if zapret still not works, try this.
-# sysctl net.ipv6.conf.all.disable_ipv6=0 > /dev/null;
-# sysctl net.ipv6.conf.default.disable_ipv6=0 > /dev/null;
-# sysctl net.ipv6.conf.lo.disable_ipv6=0 > /dev/null;
+sysctl net.ipv6.conf.all.disable_ipv6=0 > /dev/null;
+sysctl net.ipv6.conf.default.disable_ipv6=0 > /dev/null;
+sysctl net.ipv6.conf.lo.disable_ipv6=0 > /dev/null;
+
 sysctl net.netfilter.nf_conntrack_tcp_be_liberal=0 > /dev/null;
+
 for pid in $(pgrep -f zapret.sh); do
     kill -9 $pid
 done
 pkill nfqws
 pkill zapret
+
 iptables -t mangle -F POSTROUTING
 iptables -t mangle -F PREROUTING
 ip6tables -t mangle -F POSTROUTING
