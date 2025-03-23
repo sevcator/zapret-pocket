8d1237cc1956f2bd3cfd20a17b2bf911a7b3a321
Seva <68725282+sevcator@users.noreply.github.com>
2025-03-23 23:00:58 +0700

    Delete module/action.sh

--- module/action.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/bash
-MODPATH=/data/adb/modules/zapret
-
-echo "**********************************"
-echo "*         zapret-magisk          *"
-echo "**********************************"
-echo ! Note: The button Action toggle zapret
-echo ! To the configure, call zapret in Terminal
-su -c "$MODPATH/system/bin/zapret toggle"
