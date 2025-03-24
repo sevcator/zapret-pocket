92c214a23a61b138e0474365df54711312a8dd00
Seva <68725282+sevcator@users.noreply.github.com>
2025-03-24 15:12:57 +0700

    Update autohosts-flowseal-alt.sh

--- module/tactics/autohosts-flowseal-alt.sh
+++ module/tactics/autohosts-flowseal-alt.sh
@@ -11,5 +11,5 @@ config="$config --filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autott
 config="$config --filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/quic-google.bin --new"
 config="$config --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/quic-google.bin $autohostlist --new"
 config="$config --filter-udp=50000-50100 --ipset=$MODPATH/ipset-discord.txt --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new"
-config="$config --filter-tcp=443 $hostlist --dpi-desync=fake,split --dpi-desync-autottl=5 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq"
-config="$config --filter-tcp=443 --dpi-desync=fake,split --dpi-desync-autottl=5 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq $autohostlist --new"
+config="$config --filter-tcp=443 $hostlist --dpi-desync=fake,split --dpi-desync-autottl=5 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --new"
+config="$config --filter-tcp=443 --dpi-desync=fake,split --dpi-desync-autottl=5 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq $autohostlist"
