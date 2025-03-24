16ec8c497fdbae98a4e1f047830152347ca7dc1d
Seva <68725282+sevcator@users.noreply.github.com>
2025-03-24 14:57:00 +0700

    Update autohosts-flowseal-alt-2.sh

--- module/tactics/autohosts-flowseal-alt-2.sh
+++ module/tactics/autohosts-flowseal-alt-2.sh
@@ -11,5 +11,5 @@ config="$config --filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autott
 config="$config --filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/quic-google.bin --new"
 config="$config --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 $autohostlist --new"
 config="$config --filter-udp=50000-50100 --ipset=$MODPATH/ipset-discord.txt --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new"
-config="$config --filter-tcp=443 $hostlist --dpi-desync=split2 --dpi-desync-split-seqovl=652 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern=$MODPATH/tls.bin"
+config="$config --filter-tcp=443 $hostlist --dpi-desync=split2 --dpi-desync-split-seqovl=652 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern=$MODPATH/tls.bin --new"
 config="$config --filter-tcp=443 --dpi-desync=split2 --dpi-desync-split-seqovl=652 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern=$MODPATH/tls.bin $autohostlist --new"
