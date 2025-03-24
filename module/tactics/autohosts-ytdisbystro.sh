0fa88fb6d4d5b09457f8da8fea4e00836846b530
Seva <68725282+sevcator@users.noreply.github.com>
2025-03-24 15:13:30 +0700

    Update autohosts-ytdisbystro.sh

--- module/tactics/autohosts-ytdisbystro.sh
+++ module/tactics/autohosts-ytdisbystro.sh
@@ -15,4 +15,4 @@ config="$config --filter-tcp=443 --hostlist-domains=googlevideo.com --dpi-desync
 config="$config --filter-tcp=443 --dpi-desync=fake,split2 --dpi-desync-split-seqovl=1 --dpi-desync-split-tls=sniext --dpi-desync-fake-tls=$MODPATH/tls-google.bin $autohostlist --dpi-desync-ttl=5 --new"
 config="$config --filter-udp=443 --hostlist-domains=youtube.com,googlevideo.com,gvt1.com,play.google.com,ytimg.com,ggpht.com,jnn-pa.googleapis.com --dpi-desync=fake --dpi-desync-repeats=4 --dpi-desync-fake-quic=$MODPATH/quic-google.bin --new"
 config="$config --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 $autohostlist --new"
-config="$config --filter-udp=50000-50099 --ipset=$MODPATH/ipset-discord.txt --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=n1 --dpi-desync-fake-quic=$MODPATH/quic-google.bin --new"
+config="$config --filter-udp=50000-50099 --ipset=$MODPATH/ipset-discord.txt --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=n1 --dpi-desync-fake-quic=$MODPATH/quic-google.bin"
