c9b55094c3487c586ee97d4ac6a1a475b679e101
Seva <68725282+sevcator@users.noreply.github.com>
2025-03-24 15:05:45 +0700

    Update autohosts-ytdisbystro.sh

--- module/tactics/autohosts-ytdisbystro.sh
+++ module/tactics/autohosts-ytdisbystro.sh
@@ -7,6 +7,9 @@ hostlist="--hostlist=$MODPATH/list.txt"
 
 # Zapret Configuration
 config="--filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-fooling=md5sig $autohostlist --new"
+config="$config --filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-fooling=md5sig $hostlist --new"
+config="$config --filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-fooling=md5sig --hostlist-domains=googlevideo.com --new"
+config="$config --filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-fooling=md5sig --hostlist-domains=youtube.com,googlevideo.com,gvt1.com,play.google.com,ytimg.com,ggpht.com,jnn-pa.googleapis.com --new"
 config="$config --filter-tcp=443 $hostlist --dpi-desync=fake,split2 --dpi-desync-split-seqovl=2 --dpi-desync-split-pos=3 --dpi-desync-fake-tls=$MODPATH/tls-google.bin --dpi-desync-ttl=3 --new"
 config="$config --filter-tcp=443 --hostlist-domains=googlevideo.com --dpi-desync=split --dpi-desync-split-pos=1 --dpi-desync-fooling=badseq --dpi-desync-repeats=10 --dpi-desync-ttl=4 --new"
 config="$config --filter-tcp=443 --dpi-desync=fake,split2 --dpi-desync-split-seqovl=1 --dpi-desync-split-tls=sniext --dpi-desync-fake-tls=$MODPATH/tls-google.bin $autohostlist --dpi-desync-ttl=5 --new"
