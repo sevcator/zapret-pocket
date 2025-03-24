291eb389f6b5a601be99876a3da87b77c986f3f7
Seva <68725282+sevcator@users.noreply.github.com>
2025-03-24 14:57:56 +0700

    Update autohosts-bol-van.sh

--- module/tactics/autohosts-bol-van.sh
+++ module/tactics/autohosts-bol-van.sh
@@ -7,6 +7,7 @@ hostlist="--hostlist=$MODPATH/list.txt"
 
 # Zapret Configuration
 config="--filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig $autohostlist --new"
+config="$config --filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig $hostlist --new"
 config="$config --filter-tcp=443 $hostlist --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=$MODPATH/tls-google.bin --new"
 config="$config --filter-tcp=443 --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq,md5sig $autohostlist --new"
 config="$config --filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=$MODPATH/quic-google.bin --new"
