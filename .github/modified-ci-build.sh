44c603f6a83a89532b6ceaac936390524ee31fb1
Seva <68725282+sevcator@users.noreply.github.com>
2025-04-03 23:13:03 +0700

    Update modified-ci-build.sh

--- .github/modified-ci-build.sh
+++ .github/modified-ci-build.sh
@@ -9,9 +9,6 @@ rm android-ndk-${NDK_VER}-linux-x86_64.zip
 NDK_TOOLS=${HOME}/android-ndk-${NDK_VER}
 export PATH=${PATH}:${NDK_TOOLS}/toolchains/llvm/prebuilt/linux-x86_64/bin
 
-OUTPUT_DIR="dnscrypt-proxy-binaries"
-mkdir -p ${OUTPUT_DIR}
-
 build_android() {
     ARCH=$1
     CC=$2
@@ -20,7 +17,7 @@ build_android() {
 
     go clean
     env CC=${CC} CXX=${CXX} CGO_ENABLED=1 GOOS=android GOARCH=${GOARCH} go build -mod vendor -ldflags="-s -w"
-    mv dnscrypt-proxy ${OUTPUT_DIR}/dnscrypt-proxy-${ARCH}
+    mv dnscrypt-proxy dnscrypt-proxy-${ARCH}
 }
 
 build_android "arm" "armv7a-linux-androideabi19-clang" "armv7a-linux-androideabi19-clang++" "arm"
@@ -28,8 +25,4 @@ build_android "arm64" "aarch64-linux-android21-clang" "aarch64-linux-android21-c
 build_android "i386" "i686-linux-android19-clang" "i686-linux-android19-clang++" "386"
 build_android "x86_64" "x86_64-linux-android21-clang" "x86_64-linux-android21-clang++" "amd64"
 
-cp ../localhost.pem ${OUTPUT_DIR}/
-
 rm -rf ${NDK_TOOLS}
-
-ls -l ${OUTPUT_DIR}
