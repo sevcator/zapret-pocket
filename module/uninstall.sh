d2add175e202076344c97fd7f55131fa0ceea256
Seva <68725282+sevcator@users.noreply.github.com>
2025-04-06 15:19:16 +0700

    Add files via upload

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh
 
+# Enable IPv6
 sysctl net.ipv6.conf.all.disable_ipv6=0 > /dev/null;
 sysctl net.ipv6.conf.default.disable_ipv6=0 > /dev/null;
 sysctl net.ipv6.conf.lo.disable_ipv6=0 > /dev/null;
@@ -7,10 +8,14 @@ sysctl net.ipv6.conf.lo.disable_ipv6=0 > /dev/null;
 sysctl net.netfilter.nf_conntrack_tcp_be_liberal=0 > /dev/null;
 
 for pid in $(pgrep -f zapret.sh); do
-    kill -9 $pid
+    kill -9 "$pid"
+done
+for pid in $(pgrep -f dnscrypt.sh); do
+    kill -9 "$pid"
 done
 pkill nfqws
 pkill zapret
+pkill dnscrypt-proxy
 
 iptables -t mangle -F POSTROUTING
 iptables -t mangle -F PREROUTING
