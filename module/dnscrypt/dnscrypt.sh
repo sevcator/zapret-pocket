e7affe91123633a2e6283a1f69ffc19b23493949
Seva <68725282+sevcator@users.noreply.github.com>
2025-04-08 22:00:16 +0700

    Update dnscrypt.sh

--- module/dnscrypt/dnscrypt.sh
+++ module/dnscrypt/dnscrypt.sh
@@ -1,9 +1,24 @@
 MODPATH=/data/adb/modules/zapret
 
+call_error_notification() {
+    "$MODPATH/call-error-notification.sh" "Curl/wget not found on system"
+}
+
 while true; do
-    if ! pgrep -x "dnscrypt-proxy" > /dev/null; then
-            . "$MODPATH/dnscrypt/make-unkillable-dnscrypt.sh" &
-	    "$MODPATH/dnscrypt/dnscrypt-proxy" > /dev/null
+    if command -v curl > /dev/null 2>&1; then
+        curl -o "$MODPATH/dnscrypt/blocked-names.txt" "https://raw.githubusercontent.com/sevcator/dnscrypt-proxy-stuff/refs/heads/main/blocked-names.txt"
+        curl -o "$MODPATH/dnscrypt/cloaking-rules.txt" "https://raw.githubusercontent.com/sevcator/dnscrypt-proxy-stuff/refs/heads/main/cloaking-rules.txt"
+    elif command -v wget > /dev/null 2>&1; then
+        wget -O "$MODPATH/dnscrypt/blocked-names.txt" "https://raw.githubusercontent.com/sevcator/dnscrypt-proxy-stuff/refs/heads/main/blocked-names.txt"
+        wget -O "$MODPATH/dnscrypt/cloaking-rules.txt" "https://raw.githubusercontent.com/sevcator/dnscrypt-proxy-stuff/refs/heads/main/cloaking-rules.txt"
+    else
+        call_error_notification
     fi
+    
+    if ! pgrep -x "dnscrypt-proxy" > /dev/null; then
+        . "$MODPATH/dnscrypt/make-unkillable-dnscrypt.sh" &
+        "$MODPATH/dnscrypt/dnscrypt-proxy" > /dev/null
+    fi
+
     sleep 5
 done
