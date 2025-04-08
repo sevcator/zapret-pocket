9137a5ea7350114e1a40c52389b5af689e96c41a
Seva <68725282+sevcator@users.noreply.github.com>
2025-04-08 23:47:00 +0700

    Update zapret.sh

--- module/zapret.sh
+++ module/zapret.sh
@@ -165,6 +165,23 @@ else
     i6Mark=""
 fi
 
+set_perm() {
+  chown $2:$3 $1 || return 1
+  chmod $4 $1 || return 1
+  local CON=$5
+  [ -z $CON ] && CON=u:object_r:system_file:s0
+  chcon $CON $1 || return 1
+}
+set_perm_recursive() {
+  find $1 -type d 2>/dev/null | while read dir; do
+    set_perm $dir $2 $3 $4 $6
+  done
+  find $1 -type f -o -type l 2>/dev/null | while read file; do
+    set_perm $file $2 $3 $5 $6
+  done
+}
+set_perm_recursive "$MODPATH" 0 2000 0755 0755
+
 addMultiPort "tcp" "$tcp_ports";
 addMultiPort "udp" "$udp_ports";
 
