5d1ba3371f5462ee43ad1c9a46b8f38161dd473c
Seva <68725282+sevcator@users.noreply.github.com>
2025-04-09 20:20:49 +0700

    Delete module/log-error.sh

--- module/log-error.sh
+++
@@ -1,9 +0,0 @@
-MODPATH=/data/adb/modules/zapret
-DATE=$(date '+%Y-%m-%d %H:%M:%S')
-
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <error_message>"
-    exit 1
-fi
-
-echo "[$DATE] $1" >> "$MODPATH/error.log"
