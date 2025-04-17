f77d6db7a25ca1bfd824e5baf6a4da6fdc261194
Seva <68725282+sevcator@users.noreply.github.com>
2025-04-17 09:59:40 +0700

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -68,26 +68,11 @@ binary_by_architecture() {
 check_requirements
 binary_by_architecture
 
-for pid in $(pgrep -f zapret.sh); do
-    kill -9 "$pid"
-done
-for pid in $(pgrep -f dnscrypt.sh); do
-    kill -9 "$pid"
-done
-pkill nfqws
-pkill zapret
-pkill dnscrypt-proxy
-
-iptables -t mangle -F POSTROUTING
-iptables -t mangle -F PREROUTING
-iptables -F OUTPUT
-iptables -F FORWARD
-iptables -t nat -F OUTPUT
-iptables -t nat -F PREROUTING
-ip6tables -t mangle -F POSTROUTING
-ip6tables -t mangle -F PREROUTING
-ip6tables -F OUTPUT
-ip6tables -F FORWARD
+if [ -f "$MODPATH/uninstall.sh" ]; then
+    "$MODPATH/uninstall.sh"
+elif [ -f "$MODUPDATEPATH/uninstall.sh" ]; then
+    "$MODUPDATEPATH/uninstall.sh"
+fi
 
 if [ -d "$MODUPDATEPATH" ]; then
     ui_print "- Backing up old files"
