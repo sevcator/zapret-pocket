9686d2b3a0b3a694ff721bd717c66a7a13f686c0
Seva <68725282+sevcator@users.noreply.github.com>
2025-04-17 16:20:02 +0700

    Update dnscrypt.sh

--- module/dnscrypt/dnscrypt.sh
+++ module/dnscrypt/dnscrypt.sh
@@ -31,6 +31,11 @@ while true; do
         fi
     fi
 
+    iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to 127.0.0.2:53
+    iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to 127.0.0.2:53
+    iptables -t nat -I PREROUTING -p udp --dport 53 -j DNAT --to 127.0.0.2:53
+    iptables -t nat -I PREROUTING -p tcp --dport 53 -j DNAT --to 127.0.0.2:53
+
     if ! pgrep -x "dnscrypt-proxy" > /dev/null; then
         . "$MODPATH/dnscrypt/make-unkillable.sh" &
         "$MODPATH/dnscrypt/dnscrypt-proxy" > /dev/null
