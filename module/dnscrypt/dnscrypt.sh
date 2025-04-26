f01b85adbea1e7d40158d65ea4dfc1af738d9ab4
Seva <68725282+sevcator@users.noreply.github.com>
2025-04-26 21:50:52 +0700

    Update dnscrypt.sh

--- module/dnscrypt/dnscrypt.sh
+++ module/dnscrypt/dnscrypt.sh
@@ -1,36 +1,6 @@
 MODPATH=/data/adb/modules/zapret
 
 while true; do
-    mode=$(cat "$MODPATH/config/dnscrypt-files-mode")
-
-    if [ "$mode" = "1" ] || [ "$mode" = "2" ]; then
-        CLOAKING_RULES_LINK=$(cat "$MODPATH/config/cloaking-rules-link")
-        
-        if [ -n "$CLOAKING_RULES_LINK" ]; then
-            if command -v curl > /dev/null 2>&1; then
-                curl -fsSL -o "$MODPATH/dnscrypt/cloaking-rules.txt" "$CLOAKING_RULES_LINK"
-            elif command -v wget > /dev/null 2>&1; then
-                wget -q -O "$MODPATH/dnscrypt/cloaking-rules.txt" "$CLOAKING_RULES_LINK"
-            fi
-        else
-            echo "- Cloaking rules link is empty." > "$MODPATH/warns.log"
-        fi
-    fi
-
-    if [ "$mode" = "2" ]; then
-        BLOCKED_NAMES_LINK=$(cat "$MODPATH/config/blocked-names-link")
-        
-        if [ -n "$BLOCKED_NAMES_LINK" ]; then
-            if command -v curl > /dev/null 2>&1; then
-                curl -fsSL -o "$MODPATH/dnscrypt/blocked-names.txt" "$BLOCKED_NAMES_LINK"
-            elif command -v wget > /dev/null 2>&1; then
-                wget -q -O "$MODPATH/dnscrypt/blocked-names.txt" "$BLOCKED_NAMES_LINK"
-            fi
-        else
-            echo "- Blocked names link is empty." > "$MODPATH/warns.log"
-        fi
-    fi
-
     sysctl net.ipv6.conf.all.disable_ipv6=1 > /dev/null
     sysctl net.ipv6.conf.default.disable_ipv6=1 > /dev/null
     sysctl net.ipv6.conf.lo.disable_ipv6=1 > /dev/null
