1b7490919e81ffb802f72df03d1ba2240ea41faa
Seva <68725282+sevcator@users.noreply.github.com>
2025-04-26 21:42:22 +0700

    Update zapret.sh

--- module/zapret/zapret.sh
+++ module/zapret/zapret.sh
@@ -1,49 +1,7 @@
 MODPATH=/data/adb/modules/zapret
 
 CURRENTSTRATEGY=$(cat $MODPATH/config/current-strategy)
-source "$MODPATH/strategy/$CURRENTSTRATEGY.sh"
-
-while true; do
-    if ping -c 1 google.com &> /dev/null; then
-        break
-    else
-        sleep 1
-    fi
-done
-
-if [ "$(cat "$MODPATH/config/current-dns-mode")" = "2" ]; then
-    . "$MODPATH/dnscrypt/dnscrypt.sh" &
-else
-    for pid in $(pgrep -f dnscrypt.sh); do
-        kill -9 "$pid"
-    done
-    pkill dnscrypt-proxy
-fi
-
-if [ "$(cat $MODPATH/config/current-dns-mode)" = "1" ]; then
-    sysctl net.ipv6.conf.all.disable_ipv6=1 > /dev/null
-    sysctl net.ipv6.conf.default.disable_ipv6=1 > /dev/null
-    sysctl net.ipv6.conf.lo.disable_ipv6=1 > /dev/null
-    iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to $CURRENTDNS
-    iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to $CURRENTDNS
-    iptables -t nat -I PREROUTING -p udp --dport 53 -j DNAT --to $CURRENTDNS
-    iptables -t nat -I PREROUTING -p tcp --dport 53 -j DNAT --to $CURRENTDNS
-fi
-
-if [ "$(cat $MODPATH/config/current-advanced-rules)" = "1" ]; then
-    ip6tables -I OUTPUT -p udp --dport 53 -j DROP
-    ip6tables -I OUTPUT -p tcp --dport 53 -j DROP
-    ip6tables -I FORWARD -p udp --dport 53 -j DROP
-    ip6tables -I FORWARD -p tcp --dport 53 -j DROP
-    iptables -I OUTPUT -p udp --dport 853 -j DROP
-    iptables -I OUTPUT -p tcp --dport 853 -j DROP
-    iptables -I FORWARD -p udp --dport 853 -j DROP
-    iptables -I FORWARD -p tcp --dport 853 -j DROP
-    ip6tables -I OUTPUT -p udp --dport 853 -j DROP
-    ip6tables -I OUTPUT -p tcp --dport 853 -j DROP
-    ip6tables -I FORWARD -p udp --dport 853 -j DROP
-    ip6tables -I FORWARD -p tcp --dport 853 -j DROP
-fi
+"$MODPATH/strategy/$CURRENTSTRATEGY.sh"
 
 tcp_ports="$(echo $config | grep -oE 'filter-tcp=[0-9,-]+' | sed -e 's/.*=//g' -e 's/,/\n/g' -e 's/ /,/g' | sort -un)";
 udp_ports="$(echo $config | grep -oE 'filter-udp=[0-9,-]+' | sed -e 's/.*=//g' -e 's/,/\n/g' -e 's/ /,/g' | sort -un)";
@@ -126,23 +84,6 @@ if [ "$(cat /proc/net/ip6_tables_matches | grep -c 'mark')" != "0" ]; then
 else
     i6Mark=""
 fi
-
-set_perm() {
-  chown $2:$3 $1 || return 1
-  chmod $4 $1 || return 1
-  local CON=$5
-  [ -z $CON ] && CON=u:object_r:system_file:s0
-  chcon $CON $1 || return 1
-}
-set_perm_recursive() {
-  find $1 -type d 2>/dev/null | while read dir; do
-    set_perm $dir $2 $3 $4 $6
-  done
-  find $1 -type f -o -type l 2>/dev/null | while read file; do
-    set_perm $file $2 $3 $5 $6
-  done
-}
-set_perm_recursive "$MODPATH" 0 2000 0755 0755
 
 addMultiPort "tcp" "$tcp_ports";
 addMultiPort "udp" "$udp_ports";
