fca6fb9d9da5afa53febe6f1f7162ebf596881de
Seva <68725282+sevcator@users.noreply.github.com>
2025-05-03 15:44:37 +0700

    Update action.sh

--- module/action.sh
+++ module/action.sh
@@ -1,5 +1,9 @@
 MODPATH=/data/adb/modules/zapret
-echo "**********************************"
-echo "*             zapret             *"
-echo "**********************************"
+echo "sSSSSSs d s.   d ss.  d ss.  d sss   sss sssss"
+echo "     s  S  ~O  S    b S    b S           S    "
+echo "    s   S   `b S    P S    P S           S    "
+echo "   s    S sSSO S sS'  S sS'  S sSSs      S    "
+echo "  s     S    O S      S   S  S           S    "
+echo " s      S    O S      S    S S           S    "
+echo "sSSSSSs P    P P      P    P P sSSss     P    "
 su -c "$MODPATH/system/bin/zapret toggle"
