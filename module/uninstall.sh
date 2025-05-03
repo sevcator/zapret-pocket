c55510d329a2fdcfc530e556e5c2d81c760afd48
Seva <68725282+sevcator@users.noreply.github.com>
2025-05-03 15:37:59 +0700

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
 
-sysctl net.ipv6.conf.all.disable_ipv6=0 > /dev/null;
-sysctl net.ipv6.conf.default.disable_ipv6=0 > /dev/null;
-sysctl net.ipv6.conf.lo.disable_ipv6=0 > /dev/null;
+for iface in all default lo; do
+    sysctl "net.ipv6.conf.$iface.disable_ipv6=0" > /dev/null
+done
 
 sysctl net.netfilter.nf_conntrack_tcp_be_liberal=0 > /dev/null;
 
@@ -13,9 +13,18 @@ done
 
 iptables -t mangle -F POSTROUTING
 iptables -t mangle -F PREROUTING
-iptables -t nat -S | grep -- '-p udp -m udp --dport 53 -j DNAT' | while read -r rule; do
-    iptables -t nat -D ${rule#-A }
+
+for proto in udp tcp; do
+    iptables -t nat -S | grep -- "-p $proto -m $proto --dport 53 -j DNAT" | while read -r rule; do
+        iptables -t nat -D ${rule#-A }
+    done
 done
-iptables -t nat -S | grep -- '-p tcp -m tcp --dport 53 -j DNAT' | while read -r rule; do
-    iptables -t nat -D ${rule#-A }
+
+for table in iptables ip6tables; do
+    for chain in OUTPUT FORWARD; do
+        for proto in udp tcp; do
+            $table -D "$chain" -p "$proto" --dport 53 -j DROP 2>/dev/null
+            $table -D "$chain" -p "$proto" --dport 853 -j DROP 2>/dev/null
+        done
+    done
 done
