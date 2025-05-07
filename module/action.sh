57496643c9f72cb4c2d74ad86295ac52495cd8ef
Seva <68725282+sevcator@users.noreply.github.com>
2025-05-07 20:52:19 +0700

    Update action.sh

--- module/action.sh
+++ module/action.sh
@@ -3,7 +3,6 @@
 MODPATH="/data/adb/modules/zapret"
 
 echo "! Zapret Module for Magisk; @sevcator/zapret-magisk"
-echo ""
 echo "! This operation may take some time, please wait"
 echo "" 
 if pgrep -f "nfqws" >/dev/null 2>&1; then
