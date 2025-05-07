ea07844fae440f0483ddf4ae073718be82b59e6e
Seva <68725282+sevcator@users.noreply.github.com>
2025-05-07 20:28:42 +0700

    Update action.sh

--- module/action.sh
+++ module/action.sh
@@ -8,14 +8,12 @@ echo "! This operation may take some time, please wait"
 echo "" 
 if pgrep -f "nfqws" >/dev/null 2>&1; then
     if su -c "$MODPATH/uninstall.sh" &>/dev/null 2>&1; then
+        sleep 5
         echo "- Service stopped"
-    else
-        echo "- Failed to stop service"
     fi
 else
     if su -c "$MODPATH/service.sh" &>/dev/null 2>&1; then
+        sleep 5
         echo "- Service started"
-    else
-        echo "- Failed to start service"
     fi
 fi
