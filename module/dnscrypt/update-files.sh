f9b9d43dba3013988c64a329d2e357a699d08201
Seva <68725282+sevcator@users.noreply.github.com>
2025-05-11 22:19:12 +0700

    Update update-files.sh

--- module/dnscrypt/update-files.sh
+++ module/dnscrypt/update-files.sh
@@ -1,6 +1,7 @@
 #!/system/bin/sh
 
 MODPATH="/data/adb/modules/zapret"
+WGET_CMD=$(cat "$MODPATH/wgetpath" 2>/dev/null || echo "wget")
 CLOAKINGUPDATE=$(cat "$MODPATH/config/dnscrypt-cloaking-update" 2>/dev/null || echo "0")
 CLOAKINGRULESLINK=$(cat "$MODPATH/config/cloaking-rules-link" 2>/dev/null || echo "https://raw.githubusercontent.com/sevcator/dnscrypt-proxy-stuff/refs/heads/main/cloaking-rules.txt")
 BLOCKEDUPDATE=$(cat "$MODPATH/config/dnscrypt-blocked-update" 2>/dev/null || echo "0")
@@ -8,10 +9,10 @@ BLOCKEDNAMESLINK=$(cat "$MODPATH/config/blocked-names-link" 2>/dev/null || echo 
 
 if [ "$CLOAKINGUPDATE" = "1" ]; then
     if [ -n "$CLOAKINGRULESLINK" ]; then
-        if command -v curl > /dev/null 2>&1; then
-            curl -fsSL -o "$MODPATH/dnscrypt/cloaking-rules.txt" "$CLOAKINGRULESLINK"
+        if [ -n "$WGET_CMD" ]; then
+            $WGET_CMD -q -O "$MODPATH/dnscrypt/cloaking-rules.txt" "$CLOAKINGRULESLINK"
         else
-            echo "curl not found, can't download cloaking file" >> "$MODPATH/warns.log"
+            echo "wget command not found, can't download cloaking file" >> "$MODPATH/warns.log"
         fi
     else
         echo "Cloaking rules link not found" >> "$MODPATH/warns.log"
@@ -20,10 +21,10 @@ fi
 
 if [ "$BLOCKEDUPDATE" = "1" ]; then
     if [ -n "$BLOCKEDNAMESLINK" ]; then
-        if command -v curl > /dev/null 2>&1; then
-            curl -fsSL -o "$MODPATH/dnscrypt/blocked-names.txt" "$BLOCKEDNAMESLINK"
+        if [ -n "$WGET_CMD" ]; then
+            $WGET_CMD -q -O "$MODPATH/dnscrypt/blocked-names.txt" "$BLOCKEDNAMESLINK"
         else
-            echo "curl not found, can't download blocked-names file" >> "$MODPATH/warns.log"
+            echo "wget command not found, can't download blocked-names file" >> "$MODPATH/warns.log"
         fi
     else
         echo "Blocked names link not found" >> "$MODPATH/warns.log"
