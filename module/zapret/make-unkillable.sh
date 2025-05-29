e1d621f0583f0c6f15f496967db99f91ba9edba9
Seva <68725282+sevcator@users.noreply.github.com>
2025-05-29 19:42:26 +0700

    Update make-unkillable.sh

--- module/zapret/make-unkillable.sh
+++ module/zapret/make-unkillable.sh
@@ -1,16 +1,10 @@
-#!/system/bin/sh
-
 sleep 10
-
 SCRIPT_PIDS=$(pgrep -f "zapret.sh")
 NFQWS_PIDS=$(pgrep nfqws)
-
 ALL_PIDS="$SCRIPT_PIDS $NFQWS_PIDS"
-
 if [ -z "$ALL_PIDS" ]; then
     exit
 fi
-
 for pid in $ALL_PIDS; do
     if [ -d "/proc/$pid" ]; then
         echo -1000 > "/proc/$pid/oom_score_adj" 2>/dev/null
