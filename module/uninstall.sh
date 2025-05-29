6e7654317d058300d9c169463d3ffd4f5c3d99ca
Seva <68725282+sevcator@users.noreply.github.com>
2025-05-29 19:36:23 +0700

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,25 +1,19 @@
 #!/system/bin/sh
-
 for iface in all default lo; do
     sysctl "net.ipv6.conf.$iface.disable_ipv6=0" > /dev/null
 done
-
 sysctl net.netfilter.nf_conntrack_tcp_be_liberal=0 > /dev/null;
-
 PROCS=("zapret.sh" "zapret-main.sh" "dnscrypt.sh" "nfqws")
 for proc in "${PROCS[@]}"; do
     pkill -9 -f "$proc" 2>/dev/null
 done
-
 iptables -t mangle -F POSTROUTING
 iptables -t mangle -F PREROUTING
-
 for proto in udp tcp; do
     iptables -t nat -S | grep -- "-p $proto -m $proto --dport 53 -j DNAT" | while read -r rule; do
         iptables -t nat -D ${rule#-A }
     done
 done
-
 for table in iptables ip6tables; do
     for chain in OUTPUT FORWARD; do
         for proto in udp tcp; do
