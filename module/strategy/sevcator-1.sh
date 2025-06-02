e08066849f8838b65c2c284acb0c0c7befb06bbb
Seva <68725282+sevcator@users.noreply.github.com>
2025-06-02 20:03:49 +0700

    Create sevcator-1.sh

---
+++ module/strategy/sevcator-1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Custom configuration (1) by sevcator
+
+# Zapret Configuration
+# <-- -->
+
+# list.txt
+config="--filter-tcp=80 --hostlist=$MODPATH/list/list.txt --dpi-desync=syndata,multidisorder --dpi-desync-split-pos=method+2 --new"
+config="$config --filter-tcp=443 --hostlist=$MODPATH/list/list.txt --dpi-desync=multidisorder --dpi-desync-split-pos=2,midsld --dpi-desync-split-seqovl=1 --new"
+config="$config --filter-udp=80,443 --hostlist=$MODPATH/list/list.txt --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/fake/quic_initial_www_google_com.bin --new"
+
+# list-auto.txt
+config="$config --filter-tcp=80 --hostlist-auto=$MODPATH/list/list-auto.txt --hostlist-exclude=$MODPATH/list/list-exclude.txt --dpi-desync=syndata,multidisorder --dpi-desync-split-pos=method+2 --new"
+config="$config --filter-tcp=443 --hostlist-auto=$MODPATH/list/list-auto.txt --hostlist-exclude=$MODPATH/list/list-exclude.txt --dpi-desync=multidisorder --dpi-desync-split-pos=2,midsld --dpi-desync-split-seqovl=1 --new"
+config="$config --filter-udp=80,443 --hostlist-auto=$MODPATH/list/list-auto.txt --hostlist-exclude=$MODPATH/list/list-exclude.txt --dpi-desync=fake --dpi-desync-repeats=6 --new"
+
+# ipset.txt
+config="$config --filter-tcp=80 --ipset=$MODPATH/ipset/ipset.txt --dpi-desync=syndata,multidisorder --dpi-desync-split-pos=method+2 --new"
+config="$config --filter-tcp=443 --ipset=$MODPATH/ipset/ipset.txt --dpi-desync=multidisorder --dpi-desync-split-pos=2,midsld --dpi-desync-split-seqovl=1 --new"
+config="$config --filter-udp=80,443 --ipset=$MODPATH/ipset/ipset.txt --dpi-desync=fake --dpi-desync-repeats=6 --new"
+
+# Discord RTC
+config="$config --filter-udp=50000-50100 --filter-l7=discord --dpi-desync=fake --dpi-desync-cutoff=n2 --dpi-desync-ttl=4"
