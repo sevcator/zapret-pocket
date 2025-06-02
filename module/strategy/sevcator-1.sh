c62b9f19bc8c87aabd3d4533d40b1b96d6f8ead1
Seva <68725282+sevcator@users.noreply.github.com>
2025-06-02 20:36:08 +0700

    Update sevcator-1.sh

--- module/strategy/sevcator-1.sh
+++ module/strategy/sevcator-1.sh
@@ -5,18 +5,18 @@
 # <-- -->
 
 # list.txt
-config="--filter-tcp=80 --hostlist=$MODPATH/list/list.txt --dpi-desync=syndata,multidisorder --dpi-desync-split-pos=method+2 --new"
-config="$config --filter-tcp=443 --hostlist=$MODPATH/list/list.txt --dpi-desync=fakeddisorder --dpi-desync-ttl=1 --dpi-desync-autottl=2 --dpi-desync-split-pos=midsld --new"
+config="--filter-tcp=80 --hostlist=$MODPATH/list/list.txt --dpi-desync=fakeddisorder --dpi-desync-ttl=1 --dpi-desync-autottl=2 --dpi-desync-split-pos=method+2 --new"
+config="$config --filter-tcp=443 --hostlist=$MODPATH/list/list.txt --dpi-desync=fake,multidisorder --dpi-desync-fooling=datanoack --dpi-desync-split-pos=1,midsld --new"
 config="$config --filter-udp=80,443 --hostlist=$MODPATH/list/list.txt --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/fake/quic_initial_www_google_com.bin --new"
 
 # list-auto.txt
-config="$config --filter-tcp=80 --hostlist-auto=$MODPATH/list/list-auto.txt --hostlist-exclude=$MODPATH/list/list-exclude.txt --dpi-desync=syndata,multidisorder --dpi-desync-split-pos=method+2 --new"
-config="$config --filter-tcp=443 --hostlist-auto=$MODPATH/list/list-auto.txt --hostlist-exclude=$MODPATH/list/list-exclude.txt --dpi-desync=fakeddisorder --dpi-desync-ttl=1 --dpi-desync-autottl=2 --dpi-desync-split-pos=midsld --new"
+config="$config --filter-tcp=80 --hostlist-auto=$MODPATH/list/list-auto.txt --hostlist-exclude=$MODPATH/list/list-exclude.txt --dpi-desync=fakeddisorder --dpi-desync-ttl=1 --dpi-desync-autottl=2 --dpi-desync-split-pos=method+2 --new"
+config="$config --filter-tcp=443 --hostlist-auto=$MODPATH/list/list-auto.txt --hostlist-exclude=$MODPATH/list/list-exclude.txt --dpi-desync=fake,multidisorder --dpi-desync-fooling=datanoack --dpi-desync-split-pos=1,midsld --new"
 config="$config --filter-udp=80,443 --hostlist-auto=$MODPATH/list/list-auto.txt --hostlist-exclude=$MODPATH/list/list-exclude.txt --dpi-desync=fake --dpi-desync-repeats=6 --new"
 
 # ipset.txt
-config="$config --filter-tcp=80 --ipset=$MODPATH/ipset/ipset.txt --dpi-desync=syndata,multidisorder --dpi-desync-split-pos=method+2 --new"
-config="$config --filter-tcp=443 --ipset=$MODPATH/ipset/ipset.txt --dpi-desync=fakeddisorder --dpi-desync-ttl=1 --dpi-desync-autottl=2 --dpi-desync-split-pos=midsld --new"
+config="$config --filter-tcp=80 --ipset=$MODPATH/ipset/ipset.txt --dpi-desync=fakeddisorder --dpi-desync-ttl=1 --dpi-desync-autottl=2 --dpi-desync-split-pos=method+2 --new"
+config="$config --filter-tcp=443 --ipset=$MODPATH/ipset/ipset.txt --dpi-desync=fake,multidisorder --dpi-desync-fooling=datanoack --dpi-desync-split-pos=1,midsld --new"
 config="$config --filter-udp=80,443 --ipset=$MODPATH/ipset/ipset.txt --dpi-desync=fake --dpi-desync-repeats=6 --new"
 
 # Discord RTC
