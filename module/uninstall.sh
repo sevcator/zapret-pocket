13ad1582a9429d92be0928d5496ea4c70ddbb4a5
Seva <68725282+sevcator@users.noreply.github.com>
2025-06-08 14:02:56 +0700

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,23 +1,21 @@
-for iface in all default lo; do
-    sysctl "net.ipv6.conf.$iface.disable_ipv6=0" > /dev/null
+for i in all default lo; do
+    sysctl "net.ipv6.conf.$i.disable_ipv6=0" >/dev/null
 done
-sysctl net.netfilter.nf_conntrack_tcp_be_liberal=0 > /dev/null;
+sysctl net.netfilter.nf_conntrack_tcp_be_liberal=0 >/dev/null
+iptables -t mangle -F
+iptables -t mangle -X
+ip6tables -t mangle -F
+ip6tables -t mangle -X
+iptables -t nat -F
+iptables -t nat -X
 PROCS=("zapret.sh" "zapret-main.sh" "dnscrypt.sh" "nfqws")
-for proc in "${PROCS[@]}"; do
-    pkill -9 -f "$proc" 2>/dev/null
-done
-iptables -t mangle -F POSTROUTING
-iptables -t mangle -F PREROUTING
-for proto in udp tcp; do
-    iptables -t nat -S | grep -- "-p $proto -m $proto --dport 53 -j DNAT" | while read -r rule; do
-        iptables -t nat -D ${rule#-A }
-    done
-done
-for table in iptables ip6tables; do
-    for chain in OUTPUT FORWARD; do
-        for proto in udp tcp; do
-            $table -D "$chain" -p "$proto" --dport 53 -j DROP 2>/dev/null
-            $table -D "$chain" -p "$proto" --dport 853 -j DROP 2>/dev/null
-        done
+for p in "${PROCS[@]}"; do
+    pids=$(pgrep -f "$p")
+    for pid in $pids; do
+        echo 0 > "/proc/$pid/oom_score_adj" 2>/dev/null
+        sleep 1
+        kill "$pid" 2>/dev/null
+        sleep 1
+        kill -9 "$pid" 2>/dev/null
     done
 done
