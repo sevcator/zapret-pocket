a683bddcfd1a19bb1d43af387ee05237ff05730c
Seva <68725282+sevcator@users.noreply.github.com>
2025-06-08 14:20:10 +0700

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,7 +1,7 @@
 for i in all default lo; do
-    sysctl "net.ipv6.conf.$i.disable_ipv6=0" >/dev/null
+    sysctl net.ipv6.conf.$i.disable_ipv6=0
 done
-sysctl net.netfilter.nf_conntrack_tcp_be_liberal=0 >/dev/null
+sysctl net.netfilter.nf_conntrack_tcp_be_liberal=0
 iptables -t mangle -F
 iptables -t mangle -X
 ip6tables -t mangle -F
@@ -12,7 +12,7 @@ PROCS=("zapret.sh" "zapret-main.sh" "dnscrypt.sh" "nfqws")
 for p in "${PROCS[@]}"; do
     pids=$(pgrep -f "$p")
     for pid in $pids; do
-        echo 0 > "/proc/$pid/oom_score_adj" 2>/dev/null
+        echo 0 > "/proc/$pid/oom_score_adj"
         sleep 1
         kill "$pid" 2>/dev/null
         sleep 1
