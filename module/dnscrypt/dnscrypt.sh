b5ebcc17e713efa6acdf58d63eee58dac6bd5cfe
Seva <68725282+sevcator@users.noreply.github.com>
2025-06-08 14:22:45 +0700

    Update dnscrypt.sh

--- module/dnscrypt/dnscrypt.sh
+++ module/dnscrypt/dnscrypt.sh
@@ -4,5 +4,9 @@ while true; do
         . "$MODPATH/dnscrypt/make-unkillable.sh" &
         "$MODPATH/dnscrypt/dnscrypt-proxy"
     fi
+    for proto in udp tcp; do
+        iptables -t nat -I OUTPUT -p "$proto" --dport 53 -j DNAT --to 127.0.0.1:5253
+        iptables -t nat -I PREROUTING -p "$proto" --dport 53 -j DNAT --to 127.0.0.1:5253
+    done
     sleep 5
 done
