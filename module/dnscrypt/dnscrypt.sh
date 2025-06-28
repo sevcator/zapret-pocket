6644c6c6347539db0e8cd7fa260fe667d9437554
Seva <68725282+sevcator@users.noreply.github.com>
2025-06-28 15:21:48 +0700

    Add files via upload

--- module/dnscrypt/dnscrypt.sh
+++ module/dnscrypt/dnscrypt.sh
@@ -1,15 +1,32 @@
+#!/system/bin/sh
 MODPATH="/data/adb/modules/zapret"
-while true; do
-    if ! pgrep -x "dnscrypt-proxy" > /dev/null; then
-        . "$MODPATH/dnscrypt/make-unkillable.sh" &
-        for proto in udp tcp; do
-            iptables -t nat -I OUTPUT -p "$proto" --dport 53 -j DNAT --to 127.0.0.1:5253
-            iptables -t nat -I PREROUTING -p "$proto" --dport 53 -j DNAT --to 127.0.0.1:5253
-        done
-        for iface in all default lo; do
-            sysctl net.ipv6.conf.$iface.disable_ipv6=1
-        done
-        "$MODPATH/dnscrypt/dnscrypt-proxy"
+rules() {
+  for proto in udp tcp; do
+    if ! iptables -t nat -C OUTPUT -p $proto --dport 53 -j REDIRECT --to-ports 5253 2>/dev/null; then
+      iptables -t nat -A OUTPUT -p $proto --dport 53 -j REDIRECT --to-ports 5253
     fi
-    sleep 5
+    if ! iptables -t nat -C PREROUTING -p $proto --dport 53 -j DNAT --to-destination 127.0.0.1:5253 2>/dev/null; then
+      iptables -t nat -A PREROUTING -p $proto --dport 53 -j DNAT --to-destination 127.0.0.1:5253
+    fi
+  done
+  for table in iptables ip6tables; do
+      for chain in OUTPUT FORWARD; do
+          for proto in udp tcp; do
+              $table -I "$chain" -p "$proto" --dport 853 -j DROP
+          done
+      done
+  done
+  for iface in all default lo; do
+    sysctl "net.ipv6.conf.$iface.disable_ipv6=1" > /dev/null 2>&1
+  done
+}
+main() {
+  "$MODPATH/dnscrypt/dnscrypt-proxy" > /dev/null 2>&1
+}
+while true; do
+  if ! pgrep -x dnscrypt-proxy > /dev/null; then
+    rules
+    main
+  fi
+  sleep 5
 done
