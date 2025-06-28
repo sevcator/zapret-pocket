1a7cd35b5eed837803b11d42ed2e0d46194ef7a1
Seva <68725282+sevcator@users.noreply.github.com>
2025-06-28 18:10:30 +0700

    Update custom.sh

--- module/strategy/custom.sh
+++ module/strategy/custom.sh
@@ -5,7 +5,7 @@
 # <-- -->
 
 # Default list
-config="$config --filter-tcp=80 --hostlist=$MODPATH/list/default.txt --dpi-desync=fake,multisplit --dpi-desync-split-pos=sld+1 --dpi-desync-fooling=md5sig --dpi-desync-autottl --new"
+config="--filter-tcp=80 --hostlist=$MODPATH/list/default.txt --dpi-desync=fake,multisplit --dpi-desync-split-pos=sld+1 --dpi-desync-fooling=md5sig --dpi-desync-autottl --new"
 config="$config --filter-tcp=443 --hostlist=$MODPATH/list/default.txt --dpi-desync=fake,split2 --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=1 --dpi-desync-fooling=badseq --dpi-desync-repeats=8 --dpi-desync-split-seqovl-pattern=$MODPATH/fake/tls_clienthello_www_google_com.bin --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --new"
 config="$config --filter-udp=80,443 --hostlist=$MODPATH/list/default.txt --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=$MODPATH/fake/quic_initial_www_google_com.bin --new"
 
