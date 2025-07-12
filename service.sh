fbc1e0bcea21ad0b15cae51356f38c3f2a4bf5e9
Seva <68725282+sevcator@users.noreply.github.com>
2025-07-12 10:21:57 +0800

    Delete service.sh

--- service.sh
+++
@@ -1,8 +0,0 @@
-boot_wait() {
-    while [[ -z $(getprop sys.boot_completed) ]]; do sleep 2; done
-}
-boot_wait
-MODPATH="/data/adb/modules/zapret"
-su -c "$MODPATH/uninstall.sh"
-sleep 2
-su -c "$MODPATH/zapret-main.sh"
