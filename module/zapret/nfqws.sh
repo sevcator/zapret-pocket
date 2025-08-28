1d4487bdcee4947ce3871914b7f241c1f09b8b2b
Seva <68725282+sevcator@users.noreply.github.com>
2025-08-28 13:20:12 +0700

    Update nfqws.sh

--- module/zapret/nfqws.sh
+++ module/zapret/nfqws.sh
@@ -2,7 +2,7 @@ MODPATH="/data/adb/modules/zapret"
 while true; do
     if ! pgrep -x "nfqws" > /dev/null; then
         . "$MODPATH/zapret/make-unkillable.sh" &
-	    "$MODPATH/zapret/nfqws" --uid=0:0 --bind-fix4 --bind-fix6 --qnum=200 $config
+	    "$MODPATH/zapret/nfqws" --uid=0:0 --bind-fix4 --bind-fix6 --qnum=200 $config > $MODPATH/zapret/latest.log
     fi
     sleep 5
 done
