f62c313dbc896976373a12d7a97d89b1109e22d3
Seva <68725282+sevcator@users.noreply.github.com>
2026-03-31 18:11:19 +0700

    Delete module directory

--- module/zapret/nfqws.sh
+++
@@ -1,8 +0,0 @@
-MODPATH="/data/adb/modules/zapret"
-while true; do
-    if ! pgrep -x "nfqws" > /dev/null; then
-        . "$MODPATH/zapret/make-unkillable.sh" &
-	    "$MODPATH/zapret/nfqws" --uid=0:0 --qnum=200 $config > "$MODPATH/zapret/latest.log"
-    fi
-    sleep 5
-done
