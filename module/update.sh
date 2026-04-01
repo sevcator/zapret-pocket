b3da0ed95777f259487423037e78061c2df301d1
sevcator <sevcator@gmail.com>
2026-04-01 21:50:07 +0700

    Remove shell line continuations

--- module/update.sh
+++ module/update.sh
@@ -8,24 +8,6 @@ ensure_default_config
 
 mkdir -p "$MODPATH/ipset"
 
-for file in \
-    "$LIST_DIR/custom.txt" \
-    "$LIST_DIR/exclude.txt" \
-    "$LIST_DIR/list-general-user.txt" \
-    "$LIST_DIR/list-exclude-user.txt" \
-    "$LIST_DIR/ipset-exclude.txt" \
-    "$LIST_DIR/ipset-exclude-user.txt" \
-    "$MODPATH/ipset/custom.txt" \
-    "$MODPATH/ipset/exclude.txt" \
-    "$MODPATH/ipset/ipset-exclude.txt" \
-    "$MODPATH/ipset/ipset-exclude-user.txt" \
-    "$DNSCRYPT_DIR/cloaking-rules.txt" \
-    "$DNSCRYPT_DIR/blocked-names.txt" \
-    "$DNSCRYPT_DIR/blocked-ips.txt" \
-    "$DNSCRYPT_DIR/custom-cloaking-rules.txt" \
-    "$DNSCRYPT_DIR/custom-blocked-names.txt" \
-    "$DNSCRYPT_DIR/custom-blocked-ips.txt" \
-    "$DNSCRYPT_DIR/custom-allowed-names.txt" \
-    "$DNSCRYPT_DIR/custom-allowed-ips.txt"; do
+for file in "$LIST_DIR/custom.txt" "$LIST_DIR/exclude.txt" "$LIST_DIR/list-general-user.txt" "$LIST_DIR/list-exclude-user.txt" "$LIST_DIR/ipset-exclude.txt" "$LIST_DIR/ipset-exclude-user.txt" "$MODPATH/ipset/custom.txt" "$MODPATH/ipset/exclude.txt" "$MODPATH/ipset/ipset-exclude.txt" "$MODPATH/ipset/ipset-exclude-user.txt" "$DNSCRYPT_DIR/cloaking-rules.txt" "$DNSCRYPT_DIR/blocked-names.txt" "$DNSCRYPT_DIR/blocked-ips.txt" "$DNSCRYPT_DIR/custom-cloaking-rules.txt" "$DNSCRYPT_DIR/custom-blocked-names.txt" "$DNSCRYPT_DIR/custom-blocked-ips.txt" "$DNSCRYPT_DIR/custom-allowed-names.txt" "$DNSCRYPT_DIR/custom-allowed-ips.txt"; do
     [ -e "$file" ] || : > "$file"
 done
